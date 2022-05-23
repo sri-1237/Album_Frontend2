1629c20452e09c8626f8da21199a1448ae716022
sri-1237 <srilakshmi.sadanala.eagles.oc.edu>
2022-05-23 17:38:12 -0500

    gh-pages

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand">Music Info</router-link>
+      <div class="navbar-nav mr-auto">
+        
+        <li class="nav-item">
+          <router-link to="/addAlbum" class="nav-link">Add New Album</router-link>
+        </li>
+      </div>
+    </nav>
+    <div class="container mt-3">
+      <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8081
+  },
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/Album_Frontend2/',
+}
+
+
